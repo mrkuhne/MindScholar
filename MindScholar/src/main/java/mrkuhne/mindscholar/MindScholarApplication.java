6b39f2044120f39813cf5591a02591d4fcb12114
Daniel Kuhne <mrkuhne@icloud.com>
2024-01-05 11:36:43 +0100

    Created the project folder

---
+++ MindScholar/src/main/java/mrkuhne/mindscholar/MindScholarApplication.java
@@ -0,0 +1,13 @@
+package mrkuhne.mindscholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindScholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindScholarApplication.class, args);
+	}
+
+}
---
+++ MindScholar/src/test/java/mrkuhne/mindscholar/MindScholarApplicationTests.java
@@ -0,0 +1,13 @@
+package mrkuhne.mindscholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindScholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
